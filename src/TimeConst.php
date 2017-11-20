8f88fd32998ce6cf670a2803b60e8345d78b778c
Vladislav Alexeyev <vlad@alexeyev.me>
2017-11-20 16:39:10 +0300

    Initial

---
+++ src/TimeConst.php
@@ -0,0 +1,25 @@
+<?php
+namespace Pinegra\TimeConst;
+
+/**
+ * Time Constants
+ * @package Pinegra\TimeConst
+ */
+final class TimeConst
+{
+    const SECOND = 1;
+    const MINUTE = 60;
+    const HOUR = 3600;
+    const DAY = 86400;
+    const WEEK = 604800;
+    const MONTH = 2592000;
+    const YEAR = 31536000;
+
+    const MONDAY = 1;
+    const TUESDAY = 2;
+    const WEDNESDAY = 3;
+    const THURSDAY = 4;
+    const WEDNESDAY = 5;
+    const SATURDAY = 6;
+    const SUNDAY = 7;
+}
